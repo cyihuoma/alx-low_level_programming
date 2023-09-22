2eb8b44af7967ba8bcf9c4b0275b0fe84d96f91a
cyihuoma <ladyihuoma@gmail.com>
2023-09-22 09:56:54 -0700

    Its been a long day

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* leet - encodes a string into 1337'
+* Letters a and A are replaced by 4.
+* Letters e and E are replaced by 3.
+* Letters o and O are replaced by 0.
+* Letters t and T are replaced by 7.
+* Letters l and L are replaced by 1.
+* @s: pointer to string.
+* Return: pointer to s.
+*/
+
+char *leet(char *s)
+{
+	int string_length, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+	/* scan through string */
+		string_length = 0;
+		while (s[string_length] != '\0')
+	/* check whether leetLetter is found */
+		{
+			leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount] == s[string_length])
+			{
+				s[string_length] = leetNums[leetCount];
+			}
+			leetCount++;
+		}
+		string_length++;
+	}
+	return (s);
+}
