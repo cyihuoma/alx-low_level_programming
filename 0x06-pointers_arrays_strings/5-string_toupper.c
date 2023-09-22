47087ebfb97a93cc6b65e4a28d0bf65ab1ff3ad8
cyihuoma <ladyihuoma@gmail.com>
2023-09-22 07:37:53 -0700

    God is love

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - allow lowercase letter
+ * @n: function parameter
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
